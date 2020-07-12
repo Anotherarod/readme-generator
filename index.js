d1bdba0b50d01df2316a63d1559ff54e1a40ee09
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-07-12 15:34:43 -0700

    Added const= variables for all the user prompts to response.data

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ async function render(){
         },
         {
             type: "input",
-            message: "What is your Project Tittle?",
-            name: "projectTittle"
+            message: "What is your Project Title?",
+            name: "projectTitle"
         },
         {
             type: "input",
@@ -48,12 +48,27 @@ async function render(){
         },
         {
             type: "input",
+            message: "please enter git hub user names of the contributor if any (If there are mulitple contributor, seperate names with comma and no space! )",
+            name: "contributorsGithubUserName"
+        },
+        {
+            type: "input",
             message: "Provide examples on how to run tests.",
             name: "tests"
         }
         ]);
         console.log(`start`);
         console.log(userResponse);
+        const gitUsername = userResponse.username;
+        const projectTitle = userResponse.projectTitle;
+        const projectDescription = userResponse.projectDescription;
+        const installationProcess = userResponse.installationProcess;
+        const instruction = userResponse.instruction;
+        const instructionExample = userResponse.instructionExample;
+        const licenseName = userResponse.licenseName;
+        const licenseUrl = userResponse.licenseUrl;
+        const contributorUserNames = userResponse.contributorsGithubUserName;
+        const tests = userResponse.tests;
 
 
 
